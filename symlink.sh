3ba0ff303eba57876cab894f844c780e99f9f5bb
kazu1192 <kazu1192@protonmail.com>
2021-07-06 15:52:58 +0900

    fix: synlink.sh unlink.sh update

--- symlink.sh
+++ symlink.sh
@@ -4,25 +4,30 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-ln -snv $SCRIPT_DIR/bin ~/.bin
-ln -snv $SCRIPT_DIR/config ~/.config
+if [ -d $HOME/.config ]; then
+  echo "move $HOME/.config directory"
+  mv $HOME/.config ${SCRIPT_DIR}/.config_tmp
+fi
 
-ln -snv $SCRIPT_DIR/bashrc ~/.bashrc
-ln -snv $SCRIPT_DIR/bash_profile ~/.bash_profile
+ln -snv ${SCRIPT_DIR}/bin $HOME/.bin
+ln -snv ${SCRIPT_DIR}/config $HOME/.config
 
-ln -snv $SCRIPT_DIR/zshrc ~/.zshrc
-ln -snv $SCRIPT_DIR/zplug.zsh ~/.zplug.zsh
+ln -snv ${SCRIPT_DIR}/bashrc $HOME/.bashrc
+ln -snv ${SCRIPT_DIR}/bash_profile $HOME/.bash_profile
 
-ln -snv $SCRIPT_DIR/vim ~/.vim
-ln -snv $SCRIPT_DIR/vimrc ~/.vimrc
+ln -snv ${SCRIPT_DIR}/zshrc $HOME/.zshrc
+ln -snv ${SCRIPT_DIR}/zplug.zsh $HOME/.zplug.zsh
 
-ln -snv $SCRIPT_DIR/tigrc ~/.tigrc
+ln -snv ${SCRIPT_DIR}/vim $HOME/.vim
+ln -snv ${SCRIPT_DIR}/vimrc $HOME/.vimrc
 
-ln -snv $SCRIPT_DIR/gitconfig ~/.gitconfig
-ln -snv $SCRIPT_DIR/gitignore ~/.gitignore
+ln -snv ${SCRIPT_DIR}/tigrc $HOME/.tigrc
 
-ln -snv $SCRIPT_DIR/tmux.conf ~/.tmux.conf
+ln -snv ${SCRIPT_DIR}/gitconfig $HOME/.gitconfig
+ln -snv ${SCRIPT_DIR}/gitignore $HOME/.gitignore
 
-ln -snv $SCRIPT_DIR/xinitrc ~/.xinitrc
+ln -snv ${SCRIPT_DIR}/tmux.conf $HOME/.tmux.conf
 
-ln -snv $SCRIPT_DIR/npmrc ~/.npmrc
+ln -snv ${SCRIPT_DIR}/xinitrc $HOME/.xinitrc
+
+ln -snv ${SCRIPT_DIR}/npmrc $HOME/.npmrc
